d6bb2aac3f6eefcf4a84236e413fc5ba9f5e9395
Duy Le <lehongduy.bk@gmail.com>
2024-03-06 10:19:52 +0700

    update code - firestore database

--- app/src/main/java/com/ph32395/lap1/HomeActivity.java
+++ app/src/main/java/com/ph32395/lap1/HomeActivity.java
@@ -11,17 +11,23 @@ import android.view.View;
 import com.airbnb.lottie.Lottie;
 import com.airbnb.lottie.LottieAnimationView;
 import com.duyle.lap1.R;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class HomeActivity extends AppCompatActivity {
 
+    FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,32 +35,11 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
 
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db = FirebaseFirestore.getInstance();
 
-        // Create a new user with a first and last name
-        Map<String, Object> user = new HashMap<>();
-        user.put("first", "Duy");
-        user.put("last", "Le");
-        user.put("born", 1915);
+        ghiDuLieu();
 
-// Add a new document with a generated ID
-        db.collection("users")
-                .add(user)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d("HomeActivity", "DocumentSnapshot added with ID: " + documentReference.getId());
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w("HomeActivity", "Error adding document", e);
-                    }
-                });
-
-//        db.collection("users").document()
-//                .addSnapshotListener()
+        docDulieu();
 
         LottieAnimationView animationView = findViewById(R.id.animationView);
 
@@ -65,6 +50,74 @@ public class HomeActivity extends AppCompatActivity {
                 Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
                 startActivity(intent);
 
+            }
+        });
+    }
+
+    private void ghiDuLieu () {
+        CollectionReference cities = db.collection("cities");
+
+        Map<String, Object> data1 = new HashMap<>();
+        data1.put("name", "San Francisco");
+        data1.put("state", "CA");
+        data1.put("country", "USA");
+        data1.put("capital", false);
+        data1.put("population", 860000);
+        data1.put("regions", Arrays.asList("west_coast", "norcal"));
+        cities.document("SF").set(data1);
+
+        Map<String, Object> data2 = new HashMap<>();
+        data2.put("name", "Los Angeles");
+        data2.put("state", "CA");
+        data2.put("country", "USA");
+        data2.put("capital", false);
+        data2.put("population", 3900000);
+        data2.put("regions", Arrays.asList("west_coast", "socal"));
+        cities.document("LA").set(data2);
+
+        Map<String, Object> data3 = new HashMap<>();
+        data3.put("name", "Washington D.C.");
+        data3.put("state", null);
+        data3.put("country", "USA");
+        data3.put("capital", true);
+        data3.put("population", 680000);
+        data3.put("regions", Arrays.asList("east_coast"));
+        cities.document("DC").set(data3);
+
+        Map<String, Object> data4 = new HashMap<>();
+        data4.put("name", "Tokyo");
+        data4.put("state", null);
+        data4.put("country", "Japan");
+        data4.put("capital", true);
+        data4.put("population", 9000000);
+        data4.put("regions", Arrays.asList("kanto", "honshu"));
+        cities.document("TOK").set(data4);
+
+        Map<String, Object> data5 = new HashMap<>();
+        data5.put("name", "Beijing");
+        data5.put("state", null);
+        data5.put("country", "China");
+        data5.put("capital", true);
+        data5.put("population", 21500000);
+        data5.put("regions", Arrays.asList("jingjinji", "hebei"));
+        cities.document("BJ").set(data5);
+    }
+
+    private void docDulieu () {
+        DocumentReference docRef = db.collection("cities").document("SF");
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d("HomeActivity", "DocumentSnapshot data: " + document.getData());
+                    } else {
+                        Log.d("HomeActivity", "No such document");
+                    }
+                } else {
+                    Log.d("HomeActivity", "get failed with ", task.getException());
+                }
             }
         });
     }
